8a020705cae1060cc4c7034989df6b539480df27
Piotrek <pchwaleba@gmail.com>
2017-04-08 19:27:36 +0200

    Add part 2 of place info layout.

--- app/src/main/java/com/example/magda/systeminformacyjny/binding/BindingAdapters.java
+++ app/src/main/java/com/example/magda/systeminformacyjny/binding/BindingAdapters.java
@@ -1,9 +1,15 @@
 package com.example.magda.systeminformacyjny.binding;
 
 import android.databinding.BindingAdapter;
+import android.media.Image;
+import android.net.Uri;
 import android.text.Html;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.example.magda.systeminformacyjny.R;
+import com.squareup.picasso.Picasso;
 
 /**
  * Created by piotrek on 08.04.17.
@@ -14,14 +20,16 @@ public class BindingAdapters {
 
     @BindingAdapter("app:text")
     public static void setText(TextView textView, String text) {
-
-        Log.d("JESTEM", text);
-
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
             textView.setText(Html.fromHtml(text, Html.FROM_HTML_MODE_LEGACY));
         } else {
             textView.setText(Html.fromHtml(text));
         }
+    }
 
+    @BindingAdapter("app:image")
+    public static void setImage(ImageView image, String url) {
+        Picasso.with(image.getContext()).load(Uri.parse(url)).placeholder(R.mipmap.ic_launcher_app)
+                .into(image);
     }
 }
--- app/src/main/java/com/example/magda/systeminformacyjny/view_models/FragmentAboutUsViewModel.java
+++ app/src/main/java/com/example/magda/systeminformacyjny/view_models/FragmentAboutUsViewModel.java
@@ -1,8 +1,30 @@
 package com.example.magda.systeminformacyjny.view_models;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
+
+import com.example.magda.systeminformacyjny.R;
+
+import static android.provider.Telephony.BaseMmsColumns.MESSAGE_TYPE;
+
 /**
  * Created by piotrek on 08.04.17.
  */
 
 public class FragmentAboutUsViewModel {
+
+    public void sendEmail(View view, Context context) {
+        final Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+        emailIntent.setType(MESSAGE_TYPE);
+        emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{context.getString(R.string.e_mail)});
+        context.startActivity(Intent.createChooser(emailIntent, context.getString(R.string.e_mail_title)));
+    }
+
+    public void showFacebook(View view, Context context) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(context.getString(R.string.facebook_url)));
+        context.startActivity(browserIntent);
+    }
+
 }
