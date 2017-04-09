d5de38e124a0b46d3861bbd4f231d55039a4aabf
Jedrzej Biedrzycki <jedrzejjanbiedrzycki@gmail.com>
2017-04-09 23:34:36 +0200

    Create LocationActivity

---
+++ app/src/main/java/com/example/magda/systeminformacyjny/activities/LocationActivity.java
@@ -0,0 +1,57 @@
+package com.example.magda.systeminformacyjny.activities;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+
+import com.example.magda.systeminformacyjny.R;
+import com.example.magda.systeminformacyjny.fragments.ActivityLocationFragment;
+
+/**
+ * Created by JB on 2017-04-09.
+ */
+
+public class LocationActivity extends FragmentActivity{
+
+    private static final int NUM_PAGES = 3;
+    private ViewPager viewPager;
+    private PagerAdapter pagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+        setContentView(R.layout.activity_location);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        pagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
+    }
+
+    public void onBackPressed() {
+        if(viewPager.getCurrentItem() == 0){
+            super.onBackPressed();
+        } else {
+            viewPager.setCurrentItem(viewPager.getCurrentItem() - 1);
+        }
+    }
+
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return new ActivityLocationFragment();
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
+    }
+}
